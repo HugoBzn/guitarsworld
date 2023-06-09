9d0a9917c8f4e644722836a5af15a174c0b92f4d
HugoBzn <hugobazan1499@gmail.com>
2023-06-09 17:57:59 -0600

    Can not existe duplicate elements

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { ref, reactive, onMounted } from 'vue';
-import { db } from './data/guitarras';
-import Guitarra from './components/Guitarra.vue';
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
+import { ref, reactive, onMounted } from "vue";
+import { db } from "./data/guitarras";
+import Guitarra from "./components/Guitarra.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 const guitarras = ref([]);
 const carrito = ref([]);
@@ -13,13 +13,18 @@ onMounted(() => {
 });
 
 const agregarCarrito = (guitarra) => {
-  guitarra.cantidad = 1;
-  carrito.value.push(guitarra);
+  const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id);
+  if (existeCarrito >= 0) {
+    carrito.value[existeCarrito].cantidad++;
+  } else {
+    guitarra.cantidad = 1;
+    carrito.value.push(guitarra);
+  }
 };
 </script>
 
 <template>
-  <Header />
+  <Header :carrito="carrito" />
 
   <main class="container-xl mt-5">
     <h2 class="text-center">Nuestra Colección</h2>
